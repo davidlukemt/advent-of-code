7261f888502dac3a3730fac7970305c03b732737
David Luke <dluke@python.home>
2022-12-05 05:59:52 +0000

    day1 part 2 complete?

--- 2022/day1/day1_part1.py
+++ 2022/day1/day1_part1.py
@@ -6,6 +6,7 @@ with open('day1_example') as f:
 inputArray = contents.splitlines()
 
 # Initialize variables
+count = 0
 elfNum = 0
 elfCal = 0
 elfTotes = []
@@ -16,16 +17,26 @@ moochTargetCal = 0
 # track the elf number and totes with the largest 
 # calorie count as mooch target
 for i in inputArray:
-    if i != '':
+    if(count == len(inputArray)-1):
         elfCal += int(i)
-    else:
+        elfTotes.append(elfCal)
+        if elfCal > moochTargetCal:
+            moochTarget = elfNum
+            moochTargetCal = elfCal
+    elif i != '':
+        elfCal += int(i)
+        count += 1
+    elif (i == ''):
         elfTotes.append(elfCal)
         if elfCal > moochTargetCal:
             moochTarget = elfNum
             moochTargetCal = elfCal
         elfNum += 1
         elfCal = 0
+        count += 1
+    
 
 # Print the mooch target and how many calories they're carrying
+print(elfTotes)
 print("The Elf to mooch from is:", moochTarget+1)
 print("They're carrying",moochTargetCal,"calories!")
---
+++ 2022/day1/day1_part2.py
@@ -0,0 +1,79 @@
+# Read in assignment input and store in string
+with open('day1_example') as f:
+    contents = f.read()
+
+# Split input lines into array
+inputArray = contents.splitlines()
+
+# Initialize variables
+count = 0
+elfNum = 0
+elfCal = 0
+elfTotes = []
+moochTarget = [0,0,0]
+moochTargetCal = [0,0,0]
+
+# Add up all elf calorie totals and store in array,
+# track the elf number and totes with the largest 
+# calorie count as mooch target
+for i in inputArray:
+    if(count == len(inputArray)-1):
+        elfCal += int(i)
+        elfTotes.append(elfCal)
+        # Check if this total is greater than the top three and update accordingly
+        if elfCal > moochTargetCal[0]:
+            moochTarget[2] = moochTarget[1]
+            moochTarget[1] = moochTarget[0]
+            moochTarget[0] = elfNum+1
+
+            moochTargetCal[2] = moochTargetCal[1]
+            moochTargetCal[1] = moochTargetCal[0]
+            moochTargetCal[0] = elfCal
+        
+        elif elfCal > moochTargetCal[1]:
+            moochTarget[2] = moochTarget[1]
+            moochTarget[1] = elfNum+1
+
+            moochTargetCal[2] = moochTargetCal[1]
+            moochTargetCal[1] = elfCal
+        
+        elif elfCal > moochTargetCal[2]:
+            moochTarget[2] = elfNum+1
+
+            moochTargetCal[2] = elfCal
+    
+    elif i != '':
+        elfCal += int(i)
+        count += 1
+    
+    elif (i == ''):
+        elfTotes.append(elfCal)
+        # Check if this total is greater than the top three and update accordingly
+        if elfCal > moochTargetCal[0]:
+            moochTarget[2] = moochTarget[1]
+            moochTarget[1] = moochTarget[0]
+            moochTarget[0] = elfNum+1
+
+            moochTargetCal[2] = moochTargetCal[1]
+            moochTargetCal[1] = moochTargetCal[0]
+            moochTargetCal[0] = elfCal
+        
+        elif elfCal > moochTargetCal[1]:
+            moochTarget[2] = moochTarget[1]
+            moochTarget[1] = elfNum+1
+
+            moochTargetCal[2] = moochTargetCal[1]
+            moochTargetCal[1] = elfCal
+        
+        elif elfCal > moochTargetCal[2]:
+            moochTarget[2] = elfNum+1
+
+            moochTargetCal[2] = elfCal
+        
+        count += 1
+        elfNum += 1
+        elfCal = 0
+
+print(elfTotes)
+print(moochTarget)
+print(moochTargetCal)
