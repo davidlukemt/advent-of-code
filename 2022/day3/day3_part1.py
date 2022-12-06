cafd37c6b3f9d05c8502d3afff021d93483380d0
David Luke <>
2022-12-06 04:37:17 +0000

    Day 3 part 1 confirmed

--- 2022/day3/day3_part1.py
+++ 2022/day3/day3_part1.py
@@ -1,5 +1,5 @@
 # Read in assignment input and store in string
-with open('day3_example') as f:
+with open('day3_input') as f:
     contents = f.read()
 
 # Split input lines into array
@@ -7,7 +7,10 @@ inputArray = contents.splitlines()
 priority = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 prioritySum = 0
 
-
+# Itterate through the list, split the input string in half and find the character that appears in each half
+# After identifing the common character, determine it's priority value based on the priority string above 
+# the characters priority is its position in the priority string starting with a in position 1
+# Add all of the priorites together
 for i in inputArray:
     half = int(len(i)/2)
     compartmentOne = i[0:half]
@@ -20,4 +23,5 @@ for i in inputArray:
             sameItem = compartmentTwo[position]
     prioritySum += priority.find(sameItem)+1
 
+# Print the sum of all priorities
 print(prioritySum)
