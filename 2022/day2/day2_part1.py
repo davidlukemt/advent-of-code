6877ecfb45b569a0d64cc4ddee58d0c1872aab0b
David Luke <>
2022-12-06 03:40:58 +0000

    Day 2 Part 1 tested

--- 2022/day2/day2_part1.py
+++ 2022/day2/day2_part1.py
@@ -1,6 +1,41 @@
 # Read in assignment input and store in string
-with open('day2_input') as f:
+with open('day2_example') as f:
     contents = f.read()
 
 # Split input lines into array
 inputArray = contents.splitlines()
+
+totalScore = 0
+
+for i in inputArray:
+    roundScore = 0
+    if i[2] == 'X':
+        roundScore += 1
+        if i[0] == 'A':
+            roundScore += 3
+        elif i[0] == 'B':
+            roundScore += 0
+        elif i[0] == 'C':
+            roundScore += 6
+    elif i[2] == 'Y':
+        roundScore += 2
+        if i[0] == 'A':
+            roundScore += 6
+        elif i[0] == 'B':
+            roundScore += 3
+        elif i[0] == 'C':
+            roundScore += 0
+    elif i[2] == 'Z':
+        roundScore += 3
+        if i[0] == 'A':
+            roundScore += 0
+        elif i[0] == 'B':
+            roundScore += 6
+        elif i[0] == 'C':
+            roundScore += 3
+    else:
+        print('Incorrect input detected')
+    print(roundScore)
+    totalScore += roundScore
+
+print(totalScore)
