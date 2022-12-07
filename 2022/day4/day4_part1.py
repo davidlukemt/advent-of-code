7477d19321b60f96501e7bd8de8f3279c1a0d71f
David Luke <>
2022-12-07 06:57:32 +0000

    Day 4 part 1 verified

--- 2022/day4/day4_part1.py
+++ 2022/day4/day4_part1.py
@@ -2,7 +2,7 @@ def myLen(e):
     return len(e)
 
 # Read in assignment input and store in string
-with open('day4_example') as f:
+with open('day4_input') as f:
     contents = f.read()
 
 # Split input lines into array
@@ -13,19 +13,21 @@ fullyContains = 0
 
 # For each pair of inputs, format them to strings of sequential numbers
 for pair in inputArray:
+    counted = 0
     pairSplit = pair.split(',')
     first = pairSplit[0].split('-')
     second = pairSplit[1].split('-')
-    
     # Check if second of the pair is contained within the first
     if int(first[0]) <= int(second[0]):
         if int(first[1]) >= int(second[1]):
             fullyContains += 1
+            counted = 1
     
     # Check if first of the pair is contained within the second
     if int(second[0]) <= int(first[0]):
         if int(second[1]) >= int(first[1]):
-            fullyContains += 1
+            if counted == 0:
+                fullyContains += 1
 
 # Return number of fully contained assignments
 print(fullyContains)
