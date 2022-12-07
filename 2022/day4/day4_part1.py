e73924d2980755db30b713ed2ed03261762f2094
David Luke <>
2022-12-07 06:21:59 +0000

    Day 4 part 1 commented

--- 2022/day4/day4_part1.py
+++ 2022/day4/day4_part1.py
@@ -8,12 +8,16 @@ with open('day4_example') as f:
 # Split input lines into array
 inputArray = contents.splitlines()
 
+# Initialize variables
 fullyContains = 0
 
+# For each pair of inputs, format them to strings of sequential numbers
 for pair in inputArray:
     split = pair.split(',')
     rangeOne = ''.join(map(str,list(range(int(split[0][0]),int((split[0][2]))+1,1))))
     rangeTwo = ''.join(map(str,list(range(int(split[1][0]),int((split[1][2]))+1,1))))
+
+    # Determine which number range is shorter and attempt to find the shorter one in the longer one if found, increment fullyContains
     if len(rangeOne) <= len(rangeTwo):
         if rangeTwo.find(rangeOne) != -1:
             fullyContains += 1
@@ -21,4 +25,5 @@ for pair in inputArray:
         if rangeOne.find(rangeTwo) != -1:
             fullyContains += 1
 
+# Return number of fully contained assignments
 print(fullyContains)
