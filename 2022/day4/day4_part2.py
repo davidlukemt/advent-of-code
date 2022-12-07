a11dd78f570b3148f5c0b6e047acbd772e804ed9
David Luke <>
2022-12-07 20:12:43 +0000

    Day 4 Part 2 commented

--- 2022/day4/day4_part2.py
+++ 2022/day4/day4_part2.py
@@ -11,13 +11,13 @@ inputArray = contents.splitlines()
 # Initialize variables
 overlap = 0
 
-# For each pair of inputs, format them to strings of sequential numbers
+# For each pair of inputs, split each input into lists fo easy reference
 for pair in inputArray:
     counted = 0
     pairSplit = pair.split(',')
     first = pairSplit[0].split('-')
     second = pairSplit[1].split('-')
-    # Check if second of the pair is contained within the first
+    # Check if any of the beginning and ends overlap with eachother
     if int(first[0]) <= int(second[0]) <= int(first[1]):
         overlap += 1
     elif int(first[0]) <= int(second[1]) <= int(first[1]):
@@ -27,5 +27,5 @@ for pair in inputArray:
     elif int(second[0]) <= int(first[1]) <= int(second[1]):
         overlap += 1
 
-# Return number of fully contained assignments
+# Return number of overlapping assignments
 print(overlap)
