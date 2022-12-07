a481712a554e62f4718804edf20f11df446cabdc
David Luke <>
2022-12-07 20:04:19 +0000

    Day 4 Part 2 tested

--- 2022/day4/day4_part2.py
+++ 2022/day4/day4_part2.py
@@ -2,14 +2,14 @@ def myLen(e):
     return len(e)
 
 # Read in assignment input and store in string
-with open('day4_input') as f:
+with open('day4_example') as f:
     contents = f.read()
 
 # Split input lines into array
 inputArray = contents.splitlines()
 
 # Initialize variables
-fullyContains = 0
+overlap = 0
 
 # For each pair of inputs, format them to strings of sequential numbers
 for pair in inputArray:
@@ -18,16 +18,14 @@ for pair in inputArray:
     first = pairSplit[0].split('-')
     second = pairSplit[1].split('-')
     # Check if second of the pair is contained within the first
-    if int(first[0]) <= int(second[0]):
-        if int(first[1]) >= int(second[1]):
-            fullyContains += 1
-            counted = 1
-    
-    # Check if first of the pair is contained within the second
-    if int(second[0]) <= int(first[0]):
-        if int(second[1]) >= int(first[1]):
-            if counted == 0:
-                fullyContains += 1
+    if int(first[0]) <= int(second[0]) <= int(first[1]):
+        overlap += 1
+    elif int(first[0]) <= int(second[1]) <= int(first[1]):
+        overlap += 1
+    elif int(second[0]) <= int(first[0]) <= int(second[1]):
+        overlap += 1
+    elif int(second[0]) <= int(first[1]) <= int(second[1]):
+        overlap += 1
 
 # Return number of fully contained assignments
-print(fullyContains)
+print(overlap)
